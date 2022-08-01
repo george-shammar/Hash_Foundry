04b3f8e2356fd203b989d35fbc72c99e8c926de4
george-shammer <george.gbenle@outlook.com>
2022-08-01 14:10:57 +0100

    Create second deployment script

--- script/second.s.sol
+++ script/second.s.sol
@@ -1,8 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-import "forge-std/Script.sol";
-import "../src/NFT.sol";
+import "../src/second.sol";
 
 contract NFTScript is Script {
     function run() external {
